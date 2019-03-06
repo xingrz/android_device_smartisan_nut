d307ec25921e326e0b987f12be3c92fa28e6607e
Roshan Pius <rpius@google.com>
2019-03-06 11:23:18 -0800

    nut: Turn off SoftAP MAC randomization
    Device does not seem to support MAC reconfiguration.
    
    Bug: 127559063
    Test: Manual verification of toggling softap on/off.
    Change-Id: I9ae25b4d14a0c207188fae2c52ac7791b2061a40

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -181,5 +181,7 @@ WIFI_DRIVER_FW_PATH_AP := "ap"
 WIFI_DRIVER_FW_PATH_STA := "sta"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
+WIFI_HIDL_FEATURE_DISABLE_AP_MAC_RANDOMIZATION := true
+
 # inherit from the proprietary version
 include vendor/smartisan/nut/BoardConfigVendor.mk
