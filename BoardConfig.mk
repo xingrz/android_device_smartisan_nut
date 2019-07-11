c690a88e4496950fe0abe219ce405bd112b43b1e
XiNGRZ <chenxingyu92@gmail.com>
2019-07-11 12:30:23 +0800

    nut: init: Load libshim_atomic.so globally
    Change-Id: Ib6990a66fcf6b7b9a80746ca0644fbb75d93dfeb

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -166,7 +166,7 @@ BOARD_SEPOLICY_DIRS += \
 
 # Shims
 TARGET_LD_SHIM_LIBS := \
-    /system/vendor/bin/mm-qcamera-daemon|libshim_camera.so:/system/vendor/bin/perfd|libshim_atomic.so \
+    /system/vendor/bin/mm-qcamera-daemon|libshim_camera.so \
     /system/vendor/lib64/libflp.so|libshims_flp.so \
     /system/vendor/lib64/libizat_core.so|libshims_get_process_name.so \
     /system/vendor/lib/libflp.so|libshims_flp.so \
