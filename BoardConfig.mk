da4f19cb9bb1a2f2f6401774c71ee7c3bb4dd3ec
XiNGRZ <chenxingyu92@gmail.com>
2019-11-10 21:22:52 +0800

    Revert "nut: init: Load libshim_atomic.so globally"
    This reverts commit c690a88e4496950fe0abe219ce405bd112b43b1e.
    
    Change-Id: I484898b588ab96e242dd512495cca6af5b9daf87

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -162,6 +162,7 @@ BOARD_SEPOLICY_DIRS += \
 # Shims
 TARGET_LD_SHIM_LIBS := \
     /system/vendor/bin/mm-qcamera-daemon|libshim_camera.so \
+    /system/vendor/bin/perfd|libshim_atomic.so \
     /system/vendor/lib/libflp.so|libshims_flp.so \
     /system/vendor/lib/libizat_core.so|libshims_get_process_name.so
 
